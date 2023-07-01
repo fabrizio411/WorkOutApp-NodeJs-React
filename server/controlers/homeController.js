c023c8951ad0db9887c0edd418f46c53d4c1d6b6
fabrizio411 <fabriziomourglia@gmail.com>
2023-07-01 20:56:47 -0300

    User sign up funcitonality

--- server/controlers/homeController.js
+++ server/controlers/homeController.js
@@ -1,11 +1,83 @@
+const mongoose = require('mongoose')
+const User = require('../models/User')
+
+
+
 // GET Index
-exports.homepage = async (req, res) => {
-    const locals = {
-        title: 'WorkOutApp',
-        description: 'Free NodeJs Notes App'
-    }
-    res.render ('index', {
+exports.homepage = async(req, res) => {
+    const locals = {title: 'WorkOutApp',}
+    res.render('index', {
         locals,
         layout: '../views/layouts/home'
     })
+}
+
+
+
+
+
+// GET Sign In
+exports.signIn = async(req, res) => {
+    const locals = {title:'Sign In | WorkOutApp'}
+
+    res.render('sign-in', {
+        locals, 
+        layout: '../views/layouts/home'
+    })
+}
+
+
+
+
+
+// GET Sign up
+exports.signUp = async(req, res) => {
+    const locals = {title:'Sign In | WorkOutApp'}
+    const errors = []
+
+    res.render('sign-up', {
+        errors,
+        locals, 
+        layout: '../views/layouts/home'
+    })
+}
+
+// POST Sign Up
+exports.signUpUserCreate = async(req, res) => {
+    const { name, email, password, confirm_password } = req.body
+    const errors = []
+    if (password !== confirm_password) {
+        errors.push({text: 'Password do not match'})
+    }
+    if (password.length < 4) {
+        errors.push({text: 'Password mus be at least 4 characters'})
+    }
+    if (errors.length > 0) {
+        res.render('sign-up', {
+            errors,
+            name, 
+            email, 
+            password, 
+            confirm_password,
+            layout: '../views/layouts/home'
+        })
+    } else {
+        const emailUser = await User.findOne({ email: email})
+        if (emailUser) {
+            errors.push({text: 'The email is already registered'})
+            res.render('sign-up', {
+                errors,
+                name, 
+                email, 
+                password, 
+                confirm_password,
+                layout: '../views/layouts/home'
+            })
+        } else {
+            const newUser = new User({name, email, password})
+            newUser.password = await newUser.encryptPassword(password)
+            await newUser.save()
+            res.redirect('/sign-in')
+        }
+    }
 }
--- server/routes/home.js
+++ server/routes/home.js
@@ -4,6 +4,9 @@ const homeController = require('../controlers/homeController')
 
 // App Routes
 router.get('/', homeController.homepage);
+router.get('/sign-in', homeController.signIn)
+router.get('/sign-up', homeController.signUp)
+router.post('/sign-up', homeController.signUpUserCreate)
 
 
 module.exports = router
