09fd2e901ed797ecc4c43f8fb603514294c7cb8c
fabrizio411 <fabriziomourglia@gmail.com>
2023-07-27 20:15:33 -0300

    Register page and conection to backend

--- client/src/pages/register/RegisterPage.jsx
+++ client/src/pages/register/RegisterPage.jsx
@@ -1,18 +1,24 @@
 import { useForm } from 'react-hook-form'
+import { registerRequest } from '../../api/auth.js'
+import '../../css/register/register.css'
 
 function RegisterPage() {
 
     const {register, handleSubmit} = useForm()
 
+    const onSubmit = handleSubmit(async (values) => {
+        const res = await registerRequest(values)
+        console.log(res)
+    })
 
 
     return (
-        <div>
-            <form onSubmit={handleSubmit((values) => console.log(values))}>
-                <input type='text' {...register('username', {required: true})}/>
-                <input type='email' {...register('email', {required: true})}/>
-                <input type='password' {...register('password', {required: true})}/>
-                <button type='submit'>Register</button>
+        <div className='page-container'>
+            <form className='form-container' onSubmit={onSubmit}>
+                <input className='input' placeholder='Username' type='text' {...register('username', {required: true})}/>
+                <input className='input' placeholder='Email' type='email' {...register('email', {required: true})}/>
+                <input className='input' placeholder='Password' type='password' {...register('password', {required: true})}/>
+                <button className='submit-btn' type='submit'>Register</button>
             </form>
         </div>
     )
