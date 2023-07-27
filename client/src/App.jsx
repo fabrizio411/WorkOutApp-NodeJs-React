1f409cf273609998b9b142361ee9b0efe417b509
fabrizio411 <fabriziomourglia@gmail.com>
2023-07-27 13:07:04 -0300

    Register and login setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,36 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import RegisterPage from './pages/register/RegisterPage'
+import LoginPage from './pages/register/LoginPage'
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<h1>Home Page</h1>}/>
-        <Route path='/login' element={<h1>login</h1>}/>
-        <Route path='/register' element={<h1>register</h1>}/>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<h1>Home Page</h1>}/>
+                <Route path='/login' element={<LoginPage/>}/>
+                <Route path='/register' element={<RegisterPage/>}/>
 
-        <Route path='/routines' element={<h1>routines</h1>}/>
-        <Route path='/create-routine' element={<h1>create routine</h1>}/>
-        <Route path='/routines/:id' element={<h1>view routine</h1>}/>
-        <Route path='/routines/edit-routine/:id' element={<h1>edit routine</h1>}/>
+                <Route path='/routines' element={<h1>routines</h1>}/>
+                <Route path='/create-routine' element={<h1>create routine</h1>}/>
+                <Route path='/routines/:id' element={<h1>view routine</h1>}/>
+                <Route path='/routines/edit-routine/:id' element={<h1>edit routine</h1>}/>
 
-        <Route path='/program' element={<h1>program</h1>}/>
+                <Route path='/program' element={<h1>program</h1>}/>
 
-        <Route path='/workout' element={<h1>workout</h1>}/>
+                <Route path='/workout' element={<h1>workout</h1>}/>
 
-        <Route path='/exercises' element={<h1>exercises</h1>}/>
+                <Route path='/exercises' element={<h1>exercises</h1>}/>
 
-        <Route path='/measures' element={<h1>measures</h1>}/>
+                <Route path='/measures' element={<h1>measures</h1>}/>
 
-        <Route path='/history' element={<h1>history</h1>}/>
+                <Route path='/history' element={<h1>history</h1>}/>
 
 
 
-      </Routes>
-    </BrowserRouter>
-  )
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ client/src/pages/register/RegisterPage.jsx
@@ -0,0 +1,21 @@
+import { useForm } from 'react-hook-form'
+
+function RegisterPage() {
+
+    const {register, handleSubmit} = useForm()
+
+
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit((values) => console.log(values))}>
+                <input type='text' {...register('username', {required: true})}/>
+                <input type='email' {...register('email', {required: true})}/>
+                <input type='password' {...register('password', {required: true})}/>
+                <button type='submit'>Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default RegisterPage
