fe13c3a5e98af28bfd013881d0c89adcc10477b0
fabrizio411 <fabriziomourglia@gmail.com>
2023-07-30 20:25:01 -0300

    Not using record model anymore

--- src/controllers/record.controller.js
+++ src/controllers/record.controller.js
@@ -48,30 +48,6 @@ export const createRecord = async (req, res) => {
             })
             const savedHistory = await newHistory.save()
             workoutRecords.push(savedHistory._id)
-
-            const recordData = await Record.findOne({ user: req.user.id, exercise: savedHistory.exercise })
-            // Update de los datos del documento Record
-            const record = await Record.findOneAndUpdate(
-                {
-                    user: req.user.id,
-                    exercise: savedHistory.exercise
-                },
-                {
-                    mainData: {
-                        total: recordData.mainData.total + savedHistory.mainData.reduce((acc, val) => acc + val),
-                        max: newRecordMax(recordData.mainData.max, savedHistory.mainData, savedHistory.id),
-                        average: newRecordAverage(recordData.mainData.average, savedHistory.mainData, recordData.mainData.averageCounter),
-                        averageCounter: recordData.mainData.averageCounter + savedHistory.mainData.length
-                    },
-                    secondaryData: {
-                        total: recordData.secondaryData.total + savedHistory.secondaryData.reduce((acc, val) => acc + val),
-                        max: newRecordMax(recordData.secondaryData.max, savedHistory.secondaryData, savedHistory.id),
-                        average: newRecordAverage(recordData.secondaryData.average, savedHistory.secondaryData, recordData.secondaryData.averageCounter),
-                        averageCounter: recordData.secondaryData.averageCounter + savedHistory.secondaryData.length
-                    }
-                },
-                {new: true}
-            ).where({ user: req.user.id })
         }
 
         const newWorkout = new Workout({
@@ -102,13 +78,6 @@ export const updateRecord = async (req, res) => {
 
         // Por cada history para actualizar 
         for (let index = 0; index < historysToUpdate.length; index++) {
-            const history = await History.findById(records[index])
-            let exerciseChange = history.exercise.toString() !== exercise[index]
-            
-            // Manejo del contador para el promedio, segun si hay cambio de ejercicio
-            let substractCounter = history.secondaryData.length
-            if (exerciseChange) substractCounter = 0
-            
             const updatedHistory = await History.findByIdAndUpdate(
                 records[index],
                 {
@@ -119,65 +88,23 @@ export const updateRecord = async (req, res) => {
                 },
                 {new: true}
             ).where({ user: req.user.id })
-
-            const recordData = await Record.findOne({ user: req.user.id, exercise: updatedHistory.exercise })
-            const record = await Record.findOneAndUpdate(
-                {
-                    user: req.user.id,
-                    exercise: updatedHistory.exercise
-                },
-                {
-                    mainData: {
-                        total: updateTotal(recordData.mainData.total, history.mainData, updatedHistory.mainData, exerciseChange),
-                        max: updateRecordMax(recordData.mainData.max, updatedHistory.mainData, updatedHistory.id),
-                        average: updateRecordAverage(recordData.mainData.average, history.mainData, updatedHistory.mainData, recordData.mainData.averageCounter),
-                        averageCounter: recordData.mainData.averageCounter - substractCounter + updatedHistory.mainData.length
-                    },
-                    secondaryData: {
-                        total: updateTotal(recordData.secondaryData.total, history.secondaryData, updatedHistory.secondaryData, exerciseChange),
-                        max: updateRecordMax(recordData.secondaryData.max, updatedHistory.secondaryData, updatedHistory.id),
-                        average: updateRecordAverage(recordData.secondaryData.average, history.secondaryData, updatedHistory.secondaryData, recordData.secondaryData.averageCounter),
-                        averageCounter: recordData.secondaryData.averageCounter - substractCounter + updatedHistory.secondaryData.length
-                    }
-                }
-            )
-
-            if (!record) return res.status(404).json({message: 'Record not found'})
-
-            // Manejo de los datos del Record asociado con el ejercicio anterior
-            if (exerciseChange) {
-                const oldRecordData = await Record.findOne({ user: req.user.id, exercise: history.exercise })
-
-                const oldRecord = await Record.findOneAndUpdate(
-                    { user: req.user.id, exercise: history.exercise },
-                    {
-                        mainData: {
-                            total: oldRecordData.mainData.total - history.mainData.reduce((acc, val) => acc + val),
-                            max: updateRecordMaxIfChanged(oldRecordData.mainData.max, history.id),
-                            average: updateRecordAverageIfChanged(oldRecordData.mainData.average, history.mainData, oldRecordData.mainData.averageCounter),
-                            averageCounter: oldRecordData.mainData.averageCounter - history.mainData.length
-                        },
-                        secondaryData: {
-                            total: oldRecordData.secondaryData.total - history.secondaryData.reduce((acc, val) => acc + val),
-                            max: updateRecordMaxIfChanged(oldRecordData.secondaryData.max, history.id),
-                            average: updateRecordAverageIfChanged(oldRecordData.secondaryData.average, history.secondaryData, oldRecordData.secondaryData.averageCounter),
-                            averageCounter: oldRecordData.secondaryData.averageCounter - history.secondaryData.length
-                        }
-                    }
-                )
-            }
         }
 
         // Update de los ID de records
+        const newRecords = []
         records.forEach((val, index) => {
-            if (val !== exercise[index]) val = exercise[index]
+            console.log(val.toString() !== exercise[index])
+            console.log(val.toString())
+            console.log( exercise[index])
+            if (val.toString() !== exercise[index]) newRecords.push(exercise[index])
+            else newRecords.push(val)
         })
 
         await Workout.findByIdAndUpdate(
             req.params.id,
             { 
                 title: req.body.title,
-                records: records 
+                records: newRecords 
             }
         )
 
@@ -202,28 +129,6 @@ export const deleteRecord = async (req, res) => {
 
         // Por cada ejercicio en workout
         for (let i = 0; i < workout.records.length; i++) {
-            const wkHistory = await History.findById(workout.records[i])
-
-            const oldRecord = await Record.findOne({ user: req.user.id, exercise: wkHistory.exercise })
-            const newRecord = await Record.findOneAndUpdate(
-                { user: req.user.id, exercise: wkHistory.exercise },
-                {
-                    mainData: {
-                        total: oldRecord.mainData.total - wkHistory.mainData.reduce((acc, val) => acc + val),
-                        max: updateRecordMaxIfChanged(oldRecord.mainData.max, wkHistory.id),
-                        average: updateRecordAverageIfChanged(oldRecord.mainData.average, wkHistory.mainData, oldRecord.mainData.averageCounter),
-                        averageCounter: oldRecord.mainData.averageCounter - wkHistory.mainData.length
-                    },
-                    secondaryData: {
-                        total: oldRecord.secondaryData.total - wkHistory.secondaryData.reduce((acc, val) => acc + val),
-                        max: updateRecordMaxIfChanged(oldRecord.secondaryData.max, wkHistory.id),
-                        average: updateRecordAverageIfChanged(oldRecord.secondaryData.average, wkHistory.secondaryData, oldRecord.secondaryData.averageCounter),
-                        averageCounter: oldRecord.secondaryData.averageCounter - wkHistory.secondaryData.length
-                    }
-                },
-                {new: true}
-            )
-
             const deletedHistory = await History.findByIdAndDelete(workout.records[i]).where({ user: req.user.id })
             if (!deletedHistory) return res.status(404).json({message: 'Record History not found'})
         }
