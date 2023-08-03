2eeedc90f8a6aa1f9d7905876de9fd5edf3b94e2
fabrizio411 <fabriziomourglia@gmail.com>
2023-08-02 21:32:09 -0300

    Create Home page content

---
+++ client/src/pages/home/HomePage.jsx
@@ -0,0 +1,49 @@
+
+
+function HomePage() {
+  return (
+    <div className="home-container">
+        <div className="header-container">
+            <h1 className="header-title">WorkOutApp</h1>
+            <p className="btn">MENU</p>
+        </div>
+
+        <section className="program-container">
+            <h2 className="section-title">Program</h2>
+            <div className="program-box">
+                <div>
+                    <h3 className="box-title">My week</h3>
+                    <p className="btn">EIDT</p>
+                </div>
+                <div className="day-display-box">
+                    <div className="display letter">
+                        <p>M</p>
+                        <p>T</p>
+                        <p>W</p>
+                        <p>T</p>
+                        <p>F</p>
+                        <p>S</p>
+                        <p>S</p>
+                    </div>
+                    <div className="display circle">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section className="for-today-container">
+            <h2 className="section-title">For Today</h2>
+            <p>Component Routine</p>
+        </section>
+    </div>
+  )
+}
+
+export default HomePage
