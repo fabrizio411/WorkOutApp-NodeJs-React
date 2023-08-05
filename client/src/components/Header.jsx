56c5a5eb3027482aa72c93d861052cc0923178c7
fabrizio411 <fabriziomourglia@gmail.com>
2023-08-05 19:58:02 -0300

    Header layout

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,21 @@
-import { NavProvider } from "../context/NavContext"
+import { NavProvider, useNav } from "../context/NavContext"
 
 function Header() {
+
+    const {isActive, setIsActive} = useNav()
+
+    const handleOpenNav = () => {
+        setIsActive(true)
+    }
+
+    console.log(isActive)
+
   return (
     <NavProvider>
         <div className='header-component-container'>
-            <div className='menu-svg-box'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path></svg>
-            </div>
+            <button className='menu-svg-btn' onClick={handleOpenNav}>
+                <svg className="menu-svg" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path></svg>
+            </button>
 
             <h1 className='title'>WorkOutApp</h1>
         </div>
