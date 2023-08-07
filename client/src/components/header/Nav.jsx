07498cd8c08edf238dc9093e6977fc4e4588f134
fabrizio411 <fabriziomourglia@gmail.com>
2023-08-07 15:50:04 -0300

    Header update

--- client/src/components/header/Nav.jsx
+++ client/src/components/header/Nav.jsx
@@ -51,6 +51,8 @@ function Nav(props) {
                 <div className='nav-container'>
                     
                     <div className='profile-info-box'>
+                        <h1 className='desktop-title'>WorkOutApp</h1>
+                        <div className='desktop-hr hr-div'></div>
                         <div className='accesibility'>
                             <button className='menu-svg-btn' onClick={handleCloseNav}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>
