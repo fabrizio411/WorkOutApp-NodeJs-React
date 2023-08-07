5352aa7fed071e7ba0e06ad6739d275dcdec5480
fabrizio411 <fabriziomourglia@gmail.com>
2023-08-07 14:22:20 -0300

    Mobile menu swipe functionality (only one page)

--- client/src/pages/calendar/CalendarPage.jsx
+++ client/src/pages/calendar/CalendarPage.jsx
@@ -1,11 +1,44 @@
+import {useSwipeable } from 'react-swipeable'
+
+import { useNav } from '../../context/NavContext'
 import Header from '../../components/header/Header'
 import Nav from '../../components/header/Nav'
+import { useEffect } from 'react'
 
 function CalendarPage() {
+
+  const {setIsActive} = useNav()
+
+  const swipeHandler = useSwipeable({
+    onSwipedRight: ({event}) => {
+      event.stopPropagation()
+      setIsActive(true)
+    },
+    onSwipedLeft: ({event}) => {
+      event.stopPropagation()
+      setIsActive(false)
+    }
+  })
+
+  const {ref: documentRef} = useSwipeable({
+    onSwipedRight: ({event}) => {
+      setIsActive(true)
+    },
+    onSwipedLeft: ({event}) => {
+      setIsActive(false)
+    }
+  })
+
+  useEffect(() => {
+    documentRef(document)
+    return () => documentRef({})
+  })
+
   return (
-    <div>
+    <div {...swipeHandler}>
       <Header/>
       <Nav current_page={'CALENDAR'}/>
+      <button className='btn-test' onClick={() => setIsActive(true)}>sdasda</button>
     </div>
   )
 }
