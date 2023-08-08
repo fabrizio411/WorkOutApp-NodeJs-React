0788c5175d712220bd5b6d599d3932ae038e9f88
fabrizio411 <fabriziomourglia@gmail.com>
2023-08-08 15:42:49 -0300

    LogOut popup content

--- client/README.md
+++ client/README.md
@@ -1,4 +1,5 @@
 ## TODO
 - logout popop overlay error, algunas veces vuelve a aparecer luego de cerrarlo
+- logout functinality
 
 - Rutinas: agregar algo en caso de qeu no haya ninguna
--- client/src/components/header/LogOutPopUp.jsx
+++ client/src/components/header/LogOutPopUp.jsx
@@ -11,7 +11,16 @@ function LogOutPopUp() {
   return (
     <div className={`logout-popup-component ${activePopUp ? 'active' : 'inactive'}`}>
         <button className="overlay" onClick={handleClosePopUp}></button>
-        <div className="content"></div>
+        <div className="content">
+          <div className="text-box">
+            <h3 className="alert-title">Confirmation</h3>
+            <p className="alert-text">Are you shure you want to logout?</p>
+          </div>
+          <div className="alert-buttons">
+            <button className="alert-btn cancel" onClick={handleClosePopUp}>Cancel</button>
+            <button className="alert-btn submit">Log Out</button>
+          </div>
+        </div>
     </div>
   )
 }
