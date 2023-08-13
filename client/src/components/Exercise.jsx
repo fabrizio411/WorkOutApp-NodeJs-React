5dba20e4e3964851955b35bee10fdbe7cdca68b0
fabrizio411 <fabriziomourglia@gmail.com>
2023-08-13 12:45:26 -0300

    Bug fixes

--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -28,9 +28,10 @@ function Exercise(props) {
               <p>Edit</p>
             </button>
             <div className="hr-bar"></div>
-            <button className="menu-option delete" onClick={(event) => {
+            <button className="menu-option delete" onClick={() => {
               deleteExercise(props.id)
               getExercises()
+              handleCloseMenu()
             }}>
               <svg className="delete-svg" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>
               <p>Delete</p>
